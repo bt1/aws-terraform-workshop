30266406ff480ce2dec5b5935acd33e989af66e9
Benny Tan <bt1@users.noreply.github.com>
2018-10-02 12:54:30 -0400

    Update main.tf

--- lab02/main.tf
+++ lab02/main.tf
@@ -1,12 +1,12 @@
-module "server-us-east-2" {
+module "server-us-east" {
   source = "./server"
-  region       = "us-east-2"
+  region       = "us-east"
   num_webs     = "${var.num_webs}"
   identity     = "${var.identity}"
-  ami          = "${lookup(var.ami, "us-east-2")}"
+  ami          = "${lookup(var.ami, "us-east")}"
   ingress_cidr = "${var.ingress_cidr}"
 }
 
 output "public_dns_us_east_2" {
-  value = "${module.server-us-east-2.public_dns}"
+  value = "${module.server-us-east.public_dns}"
 }
